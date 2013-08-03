886aab342ab8b27c8d1ff4adda31ab9c2a72326e
Cuzzo <yahn007@outlook.com>
2013-08-02 21:19:22 -0700

    Added an install file.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s .gitconfig $HOME_DIR
+ln -s .vim $HOME_DIR
+ln -s .vimrc $HOME_DIR
+
+if [ -z $BASH_IT ]; then
+	echo "bash-it not installed; bash-it customizations not installed.";
+	echo "Ensure that .bash_profile is invoked during bash startup.";
+else
+	cp -R $DOTFILES_DIR/themes/* $BASH_IT/themes
+	cp -R $DOTFILES_DIR/custom/* $BASH_IT/custom
+	echo "Remember to set \$BASH_IT_THEME in .bash_profile";
+fi
